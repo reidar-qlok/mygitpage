d329330f00d3af0702fb1115003dd188d50f97ee
reidar-qlok <reidar-nilsen@qlok.se>
2022-05-04 11:59:45 +0200

    Test2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route, Link } from "react-router";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
