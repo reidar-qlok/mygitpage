5b7bf28f9ca02a0507d9b1e5ae121a43d3adc8a9
reidar-qlok <reidar-nilsen@qlok.se>
2022-05-04 13:39:33 +0200

    Test2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route exact path="mygitpage" />
         <Route path="/mygitpage/" element={<Home />} />
         <Route path="/mygitpage/about" element={<About />} />
         <Route path="/mygitpage/privacy" element={<Privacy />} />
